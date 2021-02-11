511aa6ebfeb01b8c00aceca21fd61a23a53bb239
Jason DeCorte <jason@spraoi.ai>
2021-02-11 11:36:27 -0500

    Updated predict func; Added poetry.lock; Updated dependencies

--- model_abc/model_abc/predict.py
+++ model_abc/model_abc/predict.py
@@ -2,7 +2,12 @@
 def predict(payload: dict, model_path: str, artifacts: list, parameters: dict):
     import logging
     import random
-    random.seed()
+    import pkg_resources
+    import joblib
+
+    # Example of loading a joblib file
+    f = pkg_resources.resource_stream("model_abc", "data/bert_wp_tok_updated.joblib")
+    j = joblib.load(f)
 
     logging.info(f"{payload=}")
 
@@ -12,6 +17,7 @@ def predict(payload: dict, model_path: str, artifacts: list, parameters: dict):
 
     logging.info(f"{parameters=}")
 
+    random.seed()
     predictions = [{"entityId": x, "predictedResult": random.random()} for x in range(10)]
 
     return predictions
