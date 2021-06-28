dbc6b6601c666dcf8640441202f2b0530e7762a6
srinidhi-spraoi <srinidhi@spraoi.ai>
2021-06-28 17:17:53 +0530

    model std

---
+++ model_std/model_std/helpers.py
@@ -0,0 +1,262 @@
+import boto3
+import joblib
+import pandas as pd
+import numpy as np
+
+
+
+def download_obj_from_s3(bucket_name, key, local_file_name):
+
+    bucket = boto3.resource("s3").Bucket(bucket_name)
+    with open(local_file_name, "wb") as file_data:
+        bucket.download_fileobj(key, file_data)
+    file_data.close()
+    loaded_model = joblib.load(local_file_name)
+
+    return loaded_model
+
+def magnificent_map(df, remap_columns):
+    pd_cat = [
+        "UNKNOWN",
+        "ILL-DEFINED CONDITIONS",
+        "CONGENITAL ANOMALIES",
+        "DISEASES OF THE BLOOD",
+        "SKIN & SUBCUTANEOUS TISSUE",
+        "RESPIRATORY SYSTEM",
+        "NERVOUS SYSTEM & SENSE ORGANS",
+        "MENTAL & NERVOUS DISORDERS",
+    ]
+    sic_high = [
+        7371,
+        6321,
+        3548,
+        3444,
+        2731,
+        7514,
+        5063,
+        2297,
+        3751,
+        8231,
+        9531,
+        3586,
+        3823,
+        3672,
+        3271,
+        2865,
+        2399,
+        7819,
+        2841,
+        4011,
+        3561,
+        3363,
+        3629,
+        8999,
+        3366,
+    ]
+    sic_med = [
+        3841,
+        6371,
+        3549,
+        5722,
+        8222,
+        4522,
+        4111,
+        8631,
+        5122,
+        2022,
+        1623,
+        5141,
+        5641,
+        3496,
+        3731,
+        3674,
+        6411,
+        6153,
+        2821,
+        1521,
+        5944,
+        3545,
+        8621,
+        5099,
+        5015,
+        9224,
+        5072,
+        8042,
+        5311,
+    ]
+
+    sic_high = [str(x) for x in sic_high]
+    sic_med = [str(x) for x in sic_med]
+    cov_core = ["STD", "STDCORE", "STDBU", "STD1"]
+    cov_vol = ["STDVOL", "VS1", "VS2"]
+
+    for col in remap_columns:
+
+        if col == "Primary Diagnosis Category":
+            df.loc[:, col] = df.loc[:, col].str.upper()
+            df.loc[df[col] == "", col] = "UNKNOWN"
+            df.loc[df[col].isnull(), col] = "UNKNOWN"
+            df.loc[
+                df[col].str.startswith("MENTAL, BEHAVIORAL & NEURO"), col
+            ] = "MENTAL & NERVOUS DISORDERS"
+            df.loc[
+                df[col].str.startswith("DISEASES OF THE NERVOUS SYSTEM"), col
+            ] = "NERVOUS SYSTEM & SENSE ORGANS"
+            df.loc[
+                df[col].str.startswith("DISEASES OF THE RESPIRATORY"), col
+            ] = "RESPIRATORY SYSTEM"
+            df.loc[
+                df[col].str.startswith("DISEASES OF THE SKIN & SUBCUTANEOUS"), col
+            ] = "SKIN & SUBCUTANEOUS TISSUE"
+            df.loc[
+                df[col].str.startswith("DISEASES OF THE BLOOD & BLOOD-FORMING"), col
+            ] = "DISEASES OF THE BLOOD"
+            df.loc[
+                df[col].str.startswith("CONGENITAL MALFORMATIONS, DEFORMATIONS"), col
+            ] = "CONGENITAL ANOMALIES"
+            df.loc[~df[col].isin(pd_cat), col] = "OTHERS"
+        elif col == "SIC Code":
+            #             df.loc[:, col] = pd.to_numeric(df.loc[:, col]).fillna(-99).astype(
+            #                 int)  #to avoid float strings!
+            df.loc[:, "SIC_risk_ind"] = (
+                df.loc[:, col].replace(sic_high, "High").replace(sic_med, "Medium")
+            )
+            df.loc[~df["SIC_risk_ind"].isin(["High", "Medium"]), "SIC_risk_ind"] = "Low"
+
+        elif col == "Coverage Code":
+            df.loc[:, col] = (
+                df.loc[:, col].replace(cov_core, "STDCORE").replace(cov_vol, "STDVOL")
+            )
+            df.loc[~df[col].isin(["STDCORE", "STDVOL"]), col] = "OTHERS"
+
+    return df
+
+
+def resolve_formatting(df, date_cols, numeric_cols):
+    """
+    function to ensure correct date and numeric formatting
+    """
+    for col in list(df.columns):
+        if col in date_cols:
+            try:
+                df.loc[:, col] = pd.to_datetime(df.loc[:, col])
+            except:
+                df.loc[:, col] = pd.to_datetime(df.loc[:, col], errors="coerce")
+        elif col in numeric_cols:
+            df.loc[:, col] = pd.to_numeric(df.loc[:, col])
+
+    return df
+
+
+def get_date_diff(col1, col2, interval):
+
+    """
+    difference between dates specified by the interval in ['Y','D','M']
+    col1 and col2 are date colummns and col2 > col1
+    """
+    diff = col2 - col1
+    diff /= np.timedelta64(1, interval)
+
+    return diff
+
+
+def add_policy_tenure_to_df(df):
+    """
+    returns a df with policy tenure column appended
+    """
+    df["policy_tenure"] = get_date_diff(
+        df["Policy Effective Date"], df["Policy Termination Date"], interval="D"
+    )
+    df["policy_tenure_2"] = get_date_diff(
+        df["Policy Effective Date"], df["Received Date"], interval="D"
+    )
+    df.loc[df["policy_tenure"].isnull(), "policy_tenure"] = df.loc[
+        df["policy_tenure"].isnull(), "policy_tenure_2"
+    ]
+    df.drop("policy_tenure_2", axis=1, inplace=True)
+    df.loc[df["policy_tenure"] <= 0, "policy_tenure"] = np.nan
+
+    return df
+
+
+def add_days_rep_to_df(df):
+    """
+    returns a df with days to report column appeneded
+    """
+
+    df["days_to_report"] = get_date_diff(
+        df["Loss Date"], df["Received Date"], interval="D"
+    )
+    return df
+
+
+def add_emp_tenure_to_df(df):
+    """
+    returns a df with employment tenure column appeneded
+    """
+    df["emp_tenure"] = get_date_diff(
+        df["Insured Hire Date"], df["Loss Date"], interval="D"
+    )
+    col = "emp_tenure"
+    df.loc[(df[col].isnull()) | (df[col] <= 0), col] = np.nan
+    #     print(f"col {col}: missing= {df[col].isnull().sum()}")
+
+    return df
+
+
+def add_prognosis_days_to_df(df):
+    """
+    returns a df with prognosis days column appeneded
+    """
+
+    df["prognosis_days"] = get_date_diff(
+        df["Loss Date"], df["Duration Date"], interval="D"
+    )
+    col = "prognosis_days"
+
+    df.loc[(df[col].isnull()) | (df[col] <= 0), col] = (
+        pd.to_numeric(df.loc[(df[col].isnull()) | (df[col] <= 0), "Duration Months"])
+        * 30
+    )
+    df.loc[df[col] <= 0, col] = np.nan
+    return df
+
+
+def to_category(df, cat_cols):
+    """
+    return df with the categorical features formatted as pandas category dtype
+    """
+    for col in cat_cols:
+        df.loc[:, col] = df.loc[:, col].astype("category")
+    return df
+
+
+def test_train_match(train_template, test_data):
+    missing_cols = set(train_template.columns) - set(test_data.columns)
+    # Add a missing column in test set with default value equal to 0
+    for c in missing_cols:
+        test_data[c] = 0
+    # Ensure the order of column in the test set is in the same order than in train set
+    test_data = test_data[train_template.columns].copy()
+    return test_data
+
+
+def get_na_rows(test_data):
+    na_inds = []
+    for i in range(test_data.shape[0]):
+        if test_data.iloc[i, :].isnull().any():
+            na_inds.append("Y")
+        else:
+            na_inds.append("N")
+    test_data.loc[:, "NA_row"] = na_inds
+    return test_data
+
+
+def posterior_correction(p1_orig, p_1_train, pred):
+
+    cf = p1_orig / p_1_train
+    ncf = (1 - p1_orig) / (1 - p_1_train)
+    pcorr = (pred * cf) / (pred * cf + (1 - pred) * ncf)
+    return pcorr
+
+
---
+++ model_std/model_std/predict.py
@@ -0,0 +1,229 @@
+def predict(**kwargs):
+
+    import pandas as pd
+    import numpy as np
+    from helpers import (
+        magnificent_map,
+        resolve_formatting,
+        add_policy_tenure_to_df,
+        add_days_rep_to_df,
+        add_emp_tenure_to_df,
+        add_prognosis_days_to_df,
+        to_category,
+        test_train_match,
+        get_na_rows,
+        posterior_correction,
+        download_obj_from_s3
+    )
+    import json
+
+
+    p1_orig = 0.0159  # 1: 462/ 0: 28513
+    p_1_train = 0.1792  # 1: {0: 24995, 1: 5458}
+
+    columns = [
+        "Claim Identifier",
+        "Received Date",
+        "Loss Date",
+        "Insured Gender",
+        "Insured Age at Loss",
+        "Insured Salary Ind",
+        "Insured Hire Date",
+        "Insured Zip",
+        "Primary Diagnosis Category",
+        "Duration Date",
+        "Duration Months",
+        "SIC Code",
+        "Coverage Code",
+        "Policy Termination Date",
+        "Policy Effective Date",
+        "SS Adjustment Ind",
+        "Insured Annualized Salary",
+        "Policy Lives",
+    ]
+
+    dates = [
+        "Received Date",
+        "Loss Date",
+        "Insured Hire Date",
+        "Duration Date",
+        "Policy Effective Date",
+        "Policy Termination Date",
+    ]
+    numeric = ["Insured Age at Loss", "Insured Annualized Salary", "Policy Lives"]
+    categorical = [
+        "Insured Gender",
+        "Insured Salary Ind",
+        "Primary Diagnosis Category",
+        "SS Adjustment Ind",
+        "SIC_risk_ind",
+        "Coverage Code",
+    ]
+
+    s3_model_bucket = kwargs.get("s3_bucket")
+    artifacts = kwargs.get("artifacts")
+
+    loaded_model = download_obj_from_s3(s3_model_bucket, artifacts.get("model"),"model.joblib")
+
+    loaded_template = pd.read_csv(f"s3://{s3_model_bucket}/{artifacts.get('template')}")
+
+    magnificent_data = pd.DataFrame([kwargs.get("inputs").get("claim")])
+    test_data = magnificent_data[columns].copy()
+    test_data = magnificent_map(
+        test_data,
+        remap_columns=[
+            "Insured Salary Ind",
+            "SIC Code",
+            "Coverage Code",
+            "Primary Diagnosis Category",
+        ],
+    )
+    test_data = resolve_formatting(test_data, dates, numeric)
+    test_data = add_policy_tenure_to_df(test_data)
+    test_data = add_days_rep_to_df(test_data).copy()
+    test_data.loc[test_data["days_to_report"] < 0, "days_to_report"] = 0
+    test_data = add_emp_tenure_to_df(test_data)
+    test_data = add_prognosis_days_to_df(test_data)
+    test_data = test_data.select_dtypes(exclude=["datetime64"])
+    test_data.drop(["Insured Zip", "Duration Months"], axis=1, inplace=True)
+    test_data = to_category(test_data, categorical)
+    test_data = get_na_rows(test_data)
+
+
+
+
+    na_inds = list(test_data.pop("NA_row"))
+    test_data = pd.get_dummies(
+        test_data, columns=test_data.select_dtypes(include="category").columns
+    )
+    test_X = test_train_match(loaded_template, test_data)
+
+    predict_prob = loaded_model.predict_proba(
+        test_X.loc[[na_ind == "N" for na_ind in na_inds], :]
+    )[:, 1]
+    test_final = magnificent_data.loc[
+        :, ["Claim Identifier", "Claim Status Category"]
+    ].copy()
+    test_final.loc[[na_ind == "N" for na_ind in na_inds], "probability"] = predict_prob
+    test_final.loc[[na_ind == "Y" for na_ind in na_inds], "probability"] = np.nan
+    test_final.loc[:, "p_corrected"] = posterior_correction(
+        p1_orig, p_1_train, test_final.loc[:, "probability"]
+    )
+    test_final.loc[:, "p_labels_corrected"] = (
+        test_final.loc[:, "p_corrected"] >= 0.5
+    ).astype(int)
+    test_final.loc[[na_ind == "Y" for na_ind in na_inds], "p_labels_corrected"] = np.nan
+
+    test_final.loc[:, "p_labels_corrected"] = test_final.loc[
+        :, "p_labels_corrected"
+    ].astype("Int64")
+
+    payload_data = test_final.loc[
+        :, ["Claim Identifier", "p_corrected", "p_labels_corrected"]
+    ].copy()  # ,'Claim Status Category'
+    payload_data.columns = [
+        "claimNumber",
+        "predictedProbability",
+        "predictedValue",
+    ]  # ,'claimStatusCategory'
+    prediction_json = json.loads(payload_data.to_json(orient="records"))
+    predicted_claim = prediction_json[0] if prediction_json else None
+    return [{"inputDataSource":f"{kwargs.get('inputs').get('datasetId')}:0","entityId":kwargs.get("inputs").get("datasetId"),"predictedResult":predicted_claim}]
+
+
+#example input
+
+# print(predict(model_name="model_std",s3_bucket="spr-ml-artifacts",artifacts={"model":"prod/MLMR_STD_Fraud_Model/artifacts/model_2021-06-14.joblib","template":"prod/MLMR_STD_Fraud_Model/artifacts/template_data_2021-06-14.csv"},inputs={"claim":
+#     {
+#         "Mental Nervous Ind": None,
+#         "Recovery Amt": 0.0,
+#         "Modified RTW Date": None,
+#         "Any Occ period": None,
+#         "__root_row_number": 366,
+#         "Claim Number": "GDC-72418",
+#         "Policy Effective Date": "01/01/2017",
+#         "DOT Exertion Level (Primary)": "Unknown",
+#         "Last Payment To Date": "01/28/2021",
+#         "DOT Desc": None,
+#         "Elimination Period": None,
+#         "DOT Code": None,
+#         "Voc Rehab Outcome": None,
+#         "Policy Line of Business": "STD",
+#         "Expected Term Date": None,
+#         "Clinical End Date": None,
+#         "Voc Rehab Service Requested": None,
+#         "Policy Termination Date": None,
+#         "Any Occ Start Date": None,
+#         "Duration Months": 1,
+#         "MentalNervousDesc": None,
+#         "Closed Date": None,
+#         "Insured State": "CA",
+#         "SS Pri Award Amt": None,
+#         "Any Occ Decision Due Date": None,
+#         "Elimination Days": 0,
+#         "SS Pursue Ind": None,
+#         "Any Occ Ind": None,
+#         "Elimination Ind": None,
+#         "__row_number": 366,
+#         "Plan Benefit Pct": 0.6,
+#         "Claim Status Description": "Benefit Case Under Review",
+#         "Secondary Diagnosis Code": "M54.17",
+#         "Secondary Diagnosis Category": None,
+#         "Claim Identifier": "GDC-72418-01",
+#         "SS Pri Award Eff Date": None,
+#         "Pre-Ex Outcome": "Y",
+#         "Claim Status Category": "ACTIVE",
+#         "Primary Diagnosis Code": "M51.27",
+#         "Voc Rehab Status": None,
+#         "Claim Cause Desc": "OTHER ACCIDENT",
+#         "Insured Salary Ind": "BI-WEEKLY",
+#         "Insured Zip": "93447",
+#         "SIC Code": 7349,
+#         "First Payment From Date": "01\/28\/2021",
+#         "SS Reject Code": None,
+#         "Any Occ Decision Made Date": None,
+#         "SIC Category": None,
+#         "Insured Age at Loss": 53,
+#         "Received Date": "02/12/2021",
+#         "Secondary Diagnosis Desc": "Radiculopathy, lumbosacral region",
+#         "Voc Rehab TSA Date": None,
+#         "TSA Ind": "N",
+#         "Secondary Diagnosis Category Desc": None,
+#         "SIC Desc": "BUILDING MAINTENANCE SERVICES, NEC",
+#         "Claim State": "CA",
+#         "ThirdPartyReferralDescription": None,
+#         "Occ Code": None,
+#         "Approval Date": "02/23/2021",
+#         "SS Awarded Date": None,
+#         "Primary Diagnosis Category": "Diseases of the musculoskeletal system & connectiv",
+#         "Taxable Pct": 0,
+#         "RTW Date": None,
+#         "Eligibility Outcome": "Approved",
+#         "SS Est Start Date": None,
+#         "SS Pri Status": "Unknown",
+#         "Plan Duration Date": None,
+#         "ThirdPartyReferralIndicator": None,
+#         "Primary Diagnosis Desc": "Other intervertebral disc displacement, lumbosacra",
+#         "Duration Date": None,
+#         "SocialSecurityPrimaryAwardType": None,
+#         "Gross Benefit Ind": 52.0,
+#         "Insured Hire Date": "04/30/2012",
+#         "Occ Category": None,
+#         "SubstanceAbuseDesc": None,
+#         "Insured Gender": "M",
+#         "Any Occ Category": "Own Occ",
+#         "Loss Date": "01/28/2021",
+#         "Voc Rehab Active Status": None,
+#         "Coverage Code": "STDATP",
+#         "SS Adjustment Ind": "N",
+#         "SS Eligible Ind": "Y",
+#         "Claim Status Code": "Open",
+#         "originalValues": "{'values': {'BenefitNumber': 'GDC-72418-01', 'ClaimStatusCategory': 'Open', 'ClaimCauseDescription': 'Accident', 'InsuredGender': 'Male', 'InsuredAnnualizedSalary': '85000-85250', 'GrossBenefitIndicator': 'Weekly', 'GrossBenefit': '750-1000', 'NetBenefit': '', 'EligibilityOutcome': 'Post Approval Rules', 'BenefitCaseType': 'STD', 'CaseSize': '2000-2025'}}",
+#         "Gross Benefit": 45500.0,
+#         "Insured Annualized Salary": 85125.0,
+#         "Net Benefit": None,
+#         "Policy Lives": 2012,
+#         "Servicing RSO": "Chicago",
+#         "Nurse Cert End Date": None
+#     }
+# ,"datasetId":"spr:dataset_id"}))
