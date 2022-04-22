00c5987418dce5319a6f7fd8a4fc943f4ba58992
Jason DeCorte <jason@spraoi.ai>
2022-04-22 00:28:31 -0400

    Added new rule for entity label; Changed prediction to be at triplet level, not dataset level; Updated dependencies; Refactored code

--- model_ffm/model_ffm/predict.py
+++ model_ffm/model_ffm/predict.py
@@ -8,166 +8,175 @@ def predict(**kwargs):
     import pkg_resources
     import re
 
+    dataset_id = kwargs.get('inputs').get('datasetId')
+
     if not kwargs.get("inputs").get("columns"):
-        return [{"inputDataSource": f"{kwargs.get('inputs').get('datasetId')}:0",
-                 "entityId": kwargs.get("inputs").get("datasetId"), "predictedResult": []}]
+        return [
+            {
+                "inputDataSource": f"{dataset_id}:0",
+                "entityId": dataset_id,
+                "predictedResult": [],
+            }
+        ]
 
     # Static Mappings for the Model
     model_config = {
         "MAX_SEQ_LEN": 16,
         "PADDING": "pre",
         "label_mapping": {
-        "entity_map": {
-            "Primary": 26,
-            "UNK": 28,
-            "Spouse": 27,
-            "Child": 10,
-            "Dependent": 18,
-            "Beneficiary-1": 0,
-            "Beneficiary-2": 1,
-            "Beneficiary-3": 2,
-            "Employer": 25,
-            "CB-1": 5,
-            "CB-2": 6,
-            "Dependent-1": 19,
-            "Dependent-2": 20,
-            "Dependent-3": 21,
-            "Dependent-4": 22,
-            "Dependent-5": 23,
-            "Dependent-6": 24,
-            "Child-1": 11,
-            "Child-2": 12,
-            "Child-3": 13,
-            "Child-4": 14,
-            "Child-5": 15,
-            "Child-6": 16,
-            "Beneficiary-4": 3,
-            "Beneficiary-5": 4,
-            "CB-3": 7,
-            "CB-4": 8,
-            "CB-5": 9,
-            "Child-7": 17
+            "entity_map": {
+                "Primary": 26,
+                "UNK": 28,
+                "Spouse": 27,
+                "Child": 10,
+                "Dependent": 18,
+                "Beneficiary-1": 0,
+                "Beneficiary-2": 1,
+                "Beneficiary-3": 2,
+                "Employer": 25,
+                "CB-1": 5,
+                "CB-2": 6,
+                "Dependent-1": 19,
+                "Dependent-2": 20,
+                "Dependent-3": 21,
+                "Dependent-4": 22,
+                "Dependent-5": 23,
+                "Dependent-6": 24,
+                "Child-1": 11,
+                "Child-2": 12,
+                "Child-3": 13,
+                "Child-4": 14,
+                "Child-5": 15,
+                "Child-6": 16,
+                "Beneficiary-4": 3,
+                "Beneficiary-5": 4,
+                "CB-3": 7,
+                "CB-4": 8,
+                "CB-5": 9,
+                "Child-7": 17,
+            },
+            "product_map": {
+                "UNK": 28,
+                "LIFE": 18,
+                "ACC": 0,
+                "ADD": 1,
+                "ASOFEE": 5,
+                "STD": 26,
+                "HEALTH": 17,
+                "DEN": 14,
+                "CRIT": 13,
+                "CIW": 9,
+                "DEPLIFE": 15,
+                "ADD LIFE": 2,
+                "LTD": 21,
+                "CHADD": 6,
+                "CHLIFE": 8,
+                "CHCRIT": 7,
+                "COBRA": 10,
+                "VIS": 29,
+                "COBRAVIS": 12,
+                "SPCRIT": 24,
+                "COBRADEN": 11,
+                "EAPFEE": 16,
+                "LIFEVOL": 20,
+                "ADDVOL": 4,
+                "ADDSUP": 3,
+                "LIFESUP": 19,
+                "SPADD": 23,
+                "SPLIFE": 25,
+                "STDVOL": 27,
+                "LTDVOL": 22,
+            },
+            "header_map": {
+                "NumWorkingHoursWeek": 48,
+                "DateOfBirth": 22,
+                "FirstName": 31,
+                "Gender": 33,
+                "LastName": 42,
+                "Salary": 60,
+                "AgeYears": 9,
+                "Premium": 52,
+                "GenericInd": 35,
+                "EligibilityInd": 27,
+                "EffectiveDate": 25,
+                "TerminationDate": 65,
+                "UNK": 77,
+                "CoverageTier": 20,
+                "AccountNumber": 0,
+                "BenefitAmount": 11,
+                "Address": 2,
+                "AddressLine1": 3,
+                "BinaryResponse": 15,
+                "Carrier": 16,
+                "Action": 1,
+                "ProductPlanNameOrCode": 55,
+                "EmploymentStatus": 28,
+                "GenericDate": 34,
+                "Zip": 75,
+                "AddressLine2": 4,
+                "Country": 18,
+                "AddressLine3": 5,
+                "AdjustDeductAmt": 6,
+                "BenefitClass": 12,
+                "CoverageAmount": 19,
+                "CurrencySalary": 21,
+                "FullName": 32,
+                "BillingDivision": 14,
+                "PhoneNumber": 51,
+                "OrgName": 50,
+                "BenefitPercentage": 13,
+                "SSN": 59,
+                "MiddleInitial": 44,
+                "Relationship": 58,
+                "BeneficiaryType": 10,
+                "Product": 54,
+                "TimeFreq": 67,
+                "EligGroup": 26,
+                "Occupation": 49,
+                "NumDependents": 47,
+                "AgeDays": 7,
+                "InforceInd": 41,
+                "GuaranteeIssueInd": 37,
+                "PremiumFreq": 53,
+                "Provider": 56,
+                "WaiveReason": 73,
+                "USCity": 69,
+                "TerminationReasonCode": 66,
+                "CompanyCode": 17,
+                "GroupNumber": 36,
+                "IDNumber": 39,
+                "NotesOrDesc": 46,
+                "USCounty": 70,
+                "HireDate": 38,
+                "DriversLicense": 24,
+                "MiddleName": 45,
+                "USStateCode": 71,
+                "DisabilityInd": 23,
+                "TobaccoUserOrSmokerInd": 68,
+                "IDType": 40,
+                "SeqNumber": 63,
+                "emailAddress": 76,
+                "SalaryFreq": 62,
+                "TaxStatus": 64,
+                "Reason": 57,
+                "MaritalStatus": 43,
+                "SalaryEffectiveDate": 61,
+                "Units": 72,
+                "WorkLocation": 74,
+                "AgeGroup": 8,
+                "FLSAStatus": 29,
+                "FSAamount": 30,
+            },
         },
-        "product_map": {
-            "UNK": 28,
-            "LIFE": 18,
-            "ACC": 0,
-            "ADD": 1,
-            "ASOFEE": 5,
-            "STD": 26,
-            "HEALTH": 17,
-            "DEN": 14,
-            "CRIT": 13,
-            "CIW": 9,
-            "DEPLIFE": 15,
-            "ADD LIFE": 2,
-            "LTD": 21,
-            "CHADD": 6,
-            "CHLIFE": 8,
-            "CHCRIT": 7,
-            "COBRA": 10,
-            "VIS": 29,
-            "COBRAVIS": 12,
-            "SPCRIT": 24,
-            "COBRADEN": 11,
-            "EAPFEE": 16,
-            "LIFEVOL": 20,
-            "ADDVOL": 4,
-            "ADDSUP": 3,
-            "LIFESUP": 19,
-            "SPADD": 23,
-            "SPLIFE": 25,
-            "STDVOL": 27,
-            "LTDVOL": 22
-        },
-        "header_map": {
-            "NumWorkingHoursWeek": 48,
-            "DateOfBirth": 22,
-            "FirstName": 31,
-            "Gender": 33,
-            "LastName": 42,
-            "Salary": 60,
-            "AgeYears": 9,
-            "Premium": 52,
-            "GenericInd": 35,
-            "EligibilityInd": 27,
-            "EffectiveDate": 25,
-            "TerminationDate": 65,
-            "UNK": 77,
-            "CoverageTier": 20,
-            "AccountNumber": 0,
-            "BenefitAmount": 11,
-            "Address": 2,
-            "AddressLine1": 3,
-            "BinaryResponse": 15,
-            "Carrier": 16,
-            "Action": 1,
-            "ProductPlanNameOrCode": 55,
-            "EmploymentStatus": 28,
-            "GenericDate": 34,
-            "Zip": 75,
-            "AddressLine2": 4,
-            "Country": 18,
-            "AddressLine3": 5,
-            "AdjustDeductAmt": 6,
-            "BenefitClass": 12,
-            "CoverageAmount": 19,
-            "CurrencySalary": 21,
-            "FullName": 32,
-            "BillingDivision": 14,
-            "PhoneNumber": 51,
-            "OrgName": 50,
-            "BenefitPercentage": 13,
-            "SSN": 59,
-            "MiddleInitial": 44,
-            "Relationship": 58,
-            "BeneficiaryType": 10,
-            "Product": 54,
-            "TimeFreq": 67,
-            "EligGroup": 26,
-            "Occupation": 49,
-            "NumDependents": 47,
-            "AgeDays": 7,
-            "InforceInd": 41,
-            "GuaranteeIssueInd": 37,
-            "PremiumFreq": 53,
-            "Provider": 56,
-            "WaiveReason": 73,
-            "USCity": 69,
-            "TerminationReasonCode": 66,
-            "CompanyCode": 17,
-            "GroupNumber": 36,
-            "IDNumber": 39,
-            "NotesOrDesc": 46,
-            "USCounty": 70,
-            "HireDate": 38,
-            "DriversLicense": 24,
-            "MiddleName": 45,
-            "USStateCode": 71,
-            "DisabilityInd": 23,
-            "TobaccoUserOrSmokerInd": 68,
-            "IDType": 40,
-            "SeqNumber": 63,
-            "emailAddress": 76,
-            "SalaryFreq": 62,
-            "TaxStatus": 64,
-            "Reason": 57,
-            "MaritalStatus": 43,
-            "SalaryEffectiveDate": 61,
-            "Units": 72,
-            "WorkLocation": 74,
-            "AgeGroup": 8,
-            "FLSAStatus": 29,
-            "FSAamount": 30
-        }
-    },
     }
 
     max_seq_len = model_config.get("MAX_SEQ_LEN")
     pad = model_config.get("PADDING")
 
     # load wordPiece tokenizer
-    tokenizer = pkg_resources.resource_stream("model_ffm", "data/bert_wp_tok_updated_v2.joblib")
+    tokenizer = pkg_resources.resource_stream(
+        "model_ffm", "data/bert_wp_tok_updated_v2.joblib"
+    )
     bert_wp_loaded = joblib.load(tokenizer)
 
     # load trained model
@@ -228,29 +237,49 @@ def predict(**kwargs):
     confidences = np.hstack(
         (prod_confidence, header_confidence, entity_confidence)
     ).tolist()
+    print(f"{pred_labels=}")
+    # Apply new rule for entity 04/22/2022
+    # if predicted entity is UNK and predicted label is NOT GroupNumber or AccountNumber
+    # then set predicted entity to Primary
+    new_labels = [[p[0], p[1], "Primary" if p[2] == "UNK" and p[1] not in ["GroupNumber", "AccountNumber"] else p[2]]
+                  for p in pred_labels]  # noqa
+    res = [[(a, b) for a, b in zip(ll, cl)] for ll, cl in zip(new_labels, confidences)]  # noqa
+    print(f"{res=}")
 
-    res = []
-    for x, y in zip(pred_labels, confidences):
-        res.append([(a, b) for a, b in zip(x, y)])
-
-    pred_list = [{entity:prediction} for entity, prediction in zip(all_columns, res)]
+    pred_list = [{entity: prediction} for entity, prediction in zip(all_columns, res)]
     pattern = re.compile("^blank_header_\d+$")
-    for item in pred_list:
-        for header, pred in item.items():
-            if pattern.match(header):
-                item[header] = [('UNK',1.0),('UNK',1.0),('UNK',1.0)]
+    unknown_triplet = [("UNK", 1.0), ("UNK", 1.0), ("UNK", 1.0)]
+    prediction_list = [{header: unknown_triplet if pattern.match(header) else pred}
+                       for item in pred_list for header, pred in item.items()]
+    print(f"{prediction_list=}")
 
-    return [{"inputDataSource":f"{kwargs.get('inputs').get('datasetId')}:0","entityId":kwargs.get("inputs").get("datasetId"),"predictedResult":pred_list}]
+    return [
+        {
+            "inputDataSource": f"{dataset_id}:0",
+            "entityId": f"{dataset_id}:{list(pr.keys())[0]}",
+            "predictedResult": pr,
+        }
+        for pr in prediction_list
+    ]
 
 
-# to be deleted..eventually
-
-# columns= ["Dependent CHILD #3 SSN",
-#     "Child#1 DOB",
-#     "Child 2 DOB",
-#     "Ch1.LastName",
-#     "ACC Effective Date",
-#     "blank_header_1",
-#     "blank_header_20"]
-# columns = []
-# print(predict(model_name="model_ffm",artifacts=["data/bert_wp_tok_updated_v2.joblib"],model_path="data/FFM_new_prod_labels_v2.h5",inputs={"datasetId":"spr:dataset_id","columns":columns}))
+if __name__ == "__main__":
+    columns = [
+        "Dependent CHILD #3 SSN",
+        "Child#1 DOB",
+        "Child 2 DOB",
+        "Ch1.LastName",
+        "ACC Effective Date",
+        "Member_Information_Employee_Benefit_Class",
+        "Member_Information_Last_Name",
+        "blank_header_1",
+        "blank_header_20"
+    ]
+    # columns = []
+    results = predict(
+        model_name="model_ffm",
+        artifacts=["data/bert_wp_tok_updated_v2.joblib"],
+        model_path="data/FFM_new_prod_labels_v2.h5",
+        inputs={"datasetId": "spr:dataset_id", "columns": columns}
+    )
+    print(f"{results=}")
