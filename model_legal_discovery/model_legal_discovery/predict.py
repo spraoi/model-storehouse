4303fd0889f52c3e047dc12067b48ced3e85fe8a
tariq-mohamed <Tariq.Mohamed@FINEOS.com>
2022-05-20 06:47:46 +0400

    FIX NLTK search paths

--- model_legal_discovery/model_legal_discovery/predict.py
+++ model_legal_discovery/model_legal_discovery/predict.py
@@ -108,7 +108,8 @@ def predict(**kwargs):
         pkg_resources.Requirement.parse("model_legal_discovery"),
         "model_legal_discovery/nltk_data",
     )
-    transformers_data = "./transformers"
+    nltk.data.path.append(ntlk_data_loation)
+
     # populate folders using data from s3
     download_s3_folder("legal-disc", "nltk_data/", ntlk_data_loation)
     download_s3_folder("legal-disc", "transformers/", transformers_data)
