36929a1294f619f1e8658ad55668254e3fc9d108
tariq-mohamed <Tariq.Mohamed@FINEOS.com>
2022-10-10 00:53:41 +0400

    Update predict.py
    Added new post-pred logic. Model version bumped to 2.4.6

--- model_ffm/model_ffm/predict.py
+++ model_ffm/model_ffm/predict.py
@@ -1,5 +1,5 @@
 def predict(**kwargs):
-
+    import pandas as pd
     import numpy as np
     from inflection import humanize, underscore
     import joblib
@@ -22,168 +22,175 @@ def predict(**kwargs):
         t = 16 - len(A)
         return np.pad(A, pad_width=(t, 0), mode="constant")
 
-    # Static Mappings for the Model
-    model_config = {
-        "MAX_SEQ_LEN": 16,
-        "PADDING": "pre",
-        "label_mapping": {
-            "entity_map": {
-                "Primary": 26,
-                "Spouse": 27,
-                "UNK": 28,
-                "Child": 10,
-                "Dependent": 18,
-                "Beneficiary-1": 0,
-                "Beneficiary-2": 1,
-                "Beneficiary-3": 2,
-                "Employer": 25,
-                "CB-1": 5,
-                "CB-2": 6,
-                "Dependent-1": 19,
-                "Dependent-2": 20,
-                "Dependent-3": 21,
-                "Dependent-4": 22,
-                "Dependent-5": 23,
-                "Dependent-6": 24,
-                "Child-1": 11,
-                "Child-2": 12,
-                "Child-3": 13,
-                "Child-4": 14,
-                "Child-5": 15,
-                "Child-6": 16,
-                "Beneficiary-4": 3,
-                "Beneficiary-5": 4,
-                "CB-3": 7,
-                "CB-4": 8,
-                "CB-5": 9,
-                "Child-7": 17,
-            },
-            "product_map": {
-                "UNK": 30,
-                "LIFE": 19,
-                "ACC": 0,
-                "ADD": 1,
-                "ASOFEE": 5,
-                "STD": 28,
-                "HEALTH": 18,
-                "DEN": 14,
-                "CRIT": 13,
-                "CIW": 9,
-                "DEPLIFE": 15,
-                "ADD LIFE": 2,
-                "LTD": 22,
-                "CHADD": 6,
-                "CHLIFE": 8,
-                "CHCRIT": 7,
-                "COBRA": 10,
-                "VIS": 31,
-                "COBRAVIS": 12,
-                "SPCRIT": 25,
-                "COBRADEN": 11,
-                "EAPFEE": 16,
-                "LIFEVOL": 21,
-                "ADDVOL": 4,
-                "ADDSUP": 3,
-                "LIFESUP": 20,
-                "SPADD": 24,
-                "SPLIFE": 26,
-                "STDVOL": 29,
-                "LTDVOL": 23,
-                "FMLA": 17,
-                "STADD": 27,
-            },
-            "header_map": {
-                "NumWorkingHoursWeek": 55,
-                "DateOfBirth": 21,
-                "FirstName": 31,
-                "Gender": 36,
-                "LastName": 48,
-                "Salary": 67,
-                "Age": 7,
-                "Premium": 58,
-                "GenericInd": 38,
-                "EligibilityInd": 27,
-                "EffectiveDate": 25,
-                "TerminationDate": 74,
-                "UNK": 78,
-                "CoverageTier": 19,
-                "AccountNumber": 0,
-                "AppliedForBenefitAmount": 10,
-                "Address": 2,
-                "AddressLine1": 3,
-                "BinaryResponse": 15,
-                "Carrier": 16,
-                "Action": 1,
-                "PlanCode": 57,
-                "EmploymentStatus": 28,
-                "GenericDate": 37,
-                "Zip": 83,
-                "AddressLine2": 4,
-                "Country": 18,
-                "AddressLine3": 5,
-                "AdjustDeductAmt": 6,
-                "BenefitClass": 12,
-                "CurrencySalary": 20,
-                "FullName": 32,
-                "BillingDivision": 14,
-                "PhoneNumber": 56,
-                "GroupNumber": 40,
-                "BenefitPercentage": 13,
-                "SSN": 66,
-                "MiddleInitial": 51,
-                "Relationship": 65,
-                "BeneficiaryType": 11,
-                "Product": 61,
-                "TimeFreq": 76,
-                "EligGroup": 26,
-                "JobTitle": 47,
-                "NumDependents": 54,
-                "EOI_Amount": 24,
-                "GF_Indicator": 34,
-                "GuaranteeIssueInd": 41,
-                "GF_BenefitAmount": 33,
-                "PremiumFreq": 59,
-                "Provider": 62,
-                "WaiveReason": 81,
-                "City": 17,
-                "TerminationReasonCode": 75,
-                "NotesOrDesc": 53,
-                "USCounty": 79,
-                "HireDate": 42,
-                "DriversLicense": 23,
-                "MiddleName": 52,
-                "State": 72,
-                "DisabilityInd": 22,
-                "TobaccoUserOrSmokerInd": 77,
-                "IDType": 43,
-                "SeqNumber": 71,
-                "emailAddress": 84,
-                "SalaryFreq": 69,
-                "InforceInd": 46,
-                "TaxStatus": 73,
-                "Reason": 63,
-                "MaritalStatus": 49,
-                "SalaryEffectiveDate": 68,
-                "RehireDate": 64,
-                "Units": 80,
-                "WorkLocation": 82,
-                "AgeGroup": 8,
-                "FLSAStatus": 29,
-                "FSAamount": 30,
-                "Alt_IdentityNumber": 9,
-                "Generic_ID": 39,
-                "MemberID": 50,
-                "IdentityNumber": 44,
-                "SecondaryAccountNumber": 70,
-                "InforceAmount": 45,
-                "PriorCarrierInd": 60,
-                "GI_Amount": 35,
-            },
-        },
-    }
+    def read_objs(name):
+        res_loc = pkg_resources.resource_stream("model_ffm", "data" + name + ".joblib")
+        # with open('data/' + name + '2.4.6.pkl', 'rb') as f:
+        #     a = pickle.load(f)
+        a = joblib.load(res_loc)
+        return a
 
-    max_seq_len = model_config.get("MAX_SEQ_LEN")
-    pad = model_config.get("PADDING")
+    def filter_bank(df):
+        _mask = (df["ent"].isin(["UNK"])) & ~df["head"].isin(
+            ["GroupNumber", "AccountNumber"]
+        )
+        _mask_1 = (df["ent"].isin(["Spouse", "Child", "Dependent"])) & (
+            df["prod"].isin(["VIS", "DEN"])
+        )
+        _mask_9 = df["base"].isin(["Eligibility_DOH"])
+        _mask_8 = (df["base"].isin(["Employee_Address"])) & (df["head"] == "Zip")
+        _mask_10 = df["base"].isin(["Class/Set"])
+        _mask_13 = (df["base"].isin(["Employee_Address_3"])) & (
+            df["head"] == "AddressLine3"
+        )
+        _mask_14 = df["head"] == "GF_BenefitAmount"
+        _mask_17 = (
+            df["base"].isin(
+                [
+                    "Supplemental/Voluntary_Employee_Life_In_Force_Amount",
+                    "Supplemental/Voluntary_Employee_Life_Applied_For_Amount",
+                ]
+            )
+        ) & (df["prod"] == "LIFESUP")
+        _mask_18 = (
+            df["base"].isin(
+                [
+                    "Supplemental/Voluntary_Employee_AD&D_Applied_For_Amount",
+                    "Supplemental/Voluntary_Employee_AD&D_In_Force_Amount",
+                ]
+            )
+        ) & (df["prod"] == "ADDSUP")
+        _mask_20 = (
+            df["base"].isin(
+                [
+                    "EMPLOYEE_Voluntary/Supplemental_Coverage_LIFE_Benefit_Amount",
+                    "EMPLOYEE_Voluntary/SupplementalCoverage_LIFE_Benefit_Amount",
+                ]
+            )
+        ) & (df["prod"] == "LIFEVOL")
+        _mask_21 = (
+            df["base"].isin(["Dependent_Relationship_Dependent_Relationship"])
+        ) & (df["ent"] == "Dependent")
+        _mask_23 = (
+            (df["head"].isin(["AccountNumber"]))
+            & (df["ent"] == "Primary")
+            & (df["prod"] == "UNK")
+        )
+        _mask_24 = (
+            (
+                df["base"].isin(
+                    ["Basic_Life_In_Force_Amount", "Basic_Life_Applied_For_Amount"]
+                )
+            )
+            & (df["ent"] == "Primary")
+            & (df["prod"] == "LTD")
+        )
+        _mask_25 = (
+            (df["base"].isin(["Supplemental/Voluntary_Employee_Life_Effective_Date"]))
+            & (df["head"].isin(["EffectiveDate"]))
+            & (df["prod"] == "LIFESUP")
+        )
+        _mask_27 = (
+            (~df["prod"].isin(["UNK", "primary"]))
+            & (df["head"].isin(["BinaryResponse"]))
+            & (df["ent"] == "Primary")
+        )
+        _mask_28 = (
+            (~df["prod"].isin(["UNK"]))
+            & (df["head"].isin(["UNK"]))
+            & (df["ent"] == "Primary")
+        )
+        _mask_29 = (
+            (df["prod"].isin(["LIFESUP"]))
+            & (df["head"].isin(["Inforce Amount"]))
+            & (df["ent"] == "Primary")
+            & (
+                df["base"].isin(
+                    ["Supplemental/Voluntary_Employee_Life_In_Force_Amount"]
+                )
+            )
+        )
+        _mask_31 = (
+            (df["prod"].isin(["ADDSUP"]))
+            & (df["head"].isin(["EffectiveDate"]))
+            & (df["ent"] == "Primary")
+            & (df["base"].isin(["Supplemental/Voluntary_Employee_AD&D_Effective_Date"]))
+        )
+        _mask_32 = (
+            (df["prod"].isin(["UNK"]))
+            & (df["head"].isin(["Zip"]))
+            & (df["ent"] == "Primary")
+            & (df["base"].isin(["Employee_Address_3"]))
+        )
+        _mask_34 = (
+            (df["prod"].isin(["UNK"]))
+            & (df["head"].isin(["UNK"]))
+            & (df["ent"] == "Primary")
+        )
+        _mask_f1 = (df["ent"].isin(["Child"])) & df["prod"].isin(["CHLIFE", "CHADD"])
+
+        df.loc[:, "prod"] = df["prod"].where(~(_mask_1), other="UNK")
+        df.loc[:, "head"] = df["head"].where(~(_mask_8), other="AddressLine1")
+        df.loc[:, "head"] = df["head"].where(~(_mask_9), other="HireDate")
+        df.loc[:, "head"] = df["head"].where(~(_mask_10), other="BenefitClass")
+        df.loc[:, "head"] = df["head"].where(~(_mask_13), other="Zip")
+        df.loc[:, "head"] = df["head"].where(~(_mask_14), other="Grandfathered Amount")
+        df.loc[:, "prod"] = df["prod"].where(~(_mask_17), other="LIFEVOL")
+        df.loc[:, "prod"] = df["prod"].where(~(_mask_18), other="ADDVOL")
+        df.loc[:, "prod"] = df["prod"].where(~(_mask_20), other="LIFESUP")
+        df.loc[:, "ent"] = df["ent"].where(~(_mask_21), other="Spouse")
+        df.loc[:, "ent"] = df["ent"].where(~(_mask_23), other="UNK")
+        df.loc[:, "prod"] = df["prod"].where(~(_mask_24), other="LIFE")
+        df.loc[:, "prod"] = df["prod"].where(~(_mask_25), other="LIFEVOL")
+        df.loc[:, "head"] = df["head"].where(~(_mask_27), other="Applied For Amount")
+        df.loc[:, "ent"] = df["ent"].where(~(_mask_28), other="UNK")
+        df.loc[:, "prod"] = df["prod"].where(~(_mask_29), other="LIFEVOL")
+        df.loc[:, "prod"] = df["prod"].where(~(_mask_31), other="ADDVOL")
+        df.loc[:, "head"] = df["head"].where(~(_mask_32), other="State")
+        df.loc[:, "ent"] = df["ent"].where(~(_mask_34), other="UNK")
+        df.loc[:, "ent"] = df["ent"].where(~(_mask), other="Primary")
+        df.loc[:, "ent"] = df["ent"].where(~(_mask_f1), other="Primary")
+
+        return df
+
+    def regex_filter_bank(row):
+        pattern_dep = re.compile("^dependent_relationship_dependent.+(\d)")
+        pattern_dep1 = re.compile("^dependent(-\d)?")
+        pattern_child = re.compile("^.+(\d)")
+        pattern_emp_add = re.compile("^employee_address(_\d)?")
+
+        match_obj_emp_add = pattern_emp_add.match(row["base"].lower())
+
+        if pattern_dep.match(row["base"].lower()):
+            if (
+                pattern_dep1.match(row["ent"].lower())
+                and row["head"] == "Relationship"
+                and row["prod"] == "UNK"
+            ):
+                row["ent"] = "Child-" + str(pattern_dep.match(row["base"].lower())[1])
+
+        if row["ent"] == "Child" and row["prod"] == "UNK":
+            if pattern_child.match(row["base"]):
+                row["ent"] = "Child-" + str(pattern_child.match(row["base"].lower())[1])
+
+        if match_obj_emp_add:
+            if match_obj_emp_add[1]:
+                if match_obj_emp_add[1][1] == "1":
+                    row["head"] = "City"
+                elif match_obj_emp_add[1][1] == "2":
+                    row["head"] = "State"
+                elif match_obj_emp_add[1][1] == "3":
+                    row["head"] = "Zip"
+            else:
+                row["head"] = "AddressLine1"
+
+        return row
+
+    prod_dict = read_objs("prod_dict")
+    head_dict = read_objs("head_dict")
+    party_dict = read_objs("entity_dict")
+
+    max_seq_len = 16
+    pad = "pre"
 
     # load wordPiece tokenizer
     tokenizer = pkg_resources.resource_stream(
@@ -192,25 +199,15 @@ def predict(**kwargs):
     bert_wp_loaded = joblib.load(tokenizer)
 
     # load trained model
-    # fp = pkg_resources.resource_filename("model_ffm", "data/ffm_model_torch_v2.2.1.onnx")
-    # loaded_model = tf.keras.models.load_model(fp)
-    fp = pkg_resources.resource_filename("model_ffm", "data/ffm_model_torch_v2.2.1.onnx")
+    fp = pkg_resources.resource_filename(
+        "model_ffm", "data/ffm_model_torch_v2.2.1.onnx"
+    )
     loaded_model = onnxruntime.InferenceSession(fp)
 
     input_name = loaded_model.get_inputs()[0].name
     label_name1 = loaded_model.get_outputs()[0].name
     label_name2 = loaded_model.get_outputs()[1].name
     label_name3 = loaded_model.get_outputs()[2].name
-
-    # load categories:index mappings
-    inv_prod_dict = model_config.get("label_mapping").get("product_map")
-    prod_dict = {v: k for k, v in inv_prod_dict.items()}
-
-    inv_head_dict = model_config.get("label_mapping").get("header_map")
-    head_dict = {v: k for k, v in inv_head_dict.items()}
-
-    inv_party_dict = model_config.get("label_mapping").get("entity_map")
-    party_dict = {v: k for k, v in inv_party_dict.items()}
 
     all_columns = kwargs.get("inputs").get("columns")
 
@@ -241,7 +238,7 @@ def predict(**kwargs):
         pred.append(
             loaded_model.run([label_name1, label_name2, label_name3], {input_name: i})
         )
-    # predictions_test = loaded_model.predict(test_tokens)
+
     predictions_test = []
     predictions_test_0 = []
     predictions_test_1 = []
@@ -298,26 +295,18 @@ def predict(**kwargs):
     confidences = np.hstack(
         (prod_confidence, header_confidence, entity_confidence)
     ).tolist()
-    # print(f"{pred_labels=}")
 
-    # Apply new rule for entity 04/22/2022
-    # if predicted entity is UNK and predicted label is NOT GroupNumber or AccountNumber
-    # then set predicted entity to Primary
-    new_labels = [
-        [
-            p[0],
-            p[1],
-            "Primary"
-            if p[2] == "UNK" and p[1] not in ["GroupNumber", "AccountNumber"]
-            else p[2],
-        ]
-        for p in pred_labels  # noqa
-    ]
+    # Apply new mapping (local_build:2.4.6)
+
+    prediction_df = pd.DataFrame(pred_labels, columns=["prod", "head", "ent"])
+    prediction_df["base"] = all_columns
+    prediction_df = filter_bank(prediction_df)
+    prediction_df = prediction_df.apply(regex_filter_bank, axis=1)
+    new_labels = prediction_df.values.tolist()
     res = [
         [(a, b) for a, b in zip(ll, cl)]
         for ll, cl in zip(new_labels, confidences)  # noqa
     ]
-    # print(f"{res=}")
 
     pred_list = [{entity: prediction} for entity, prediction in zip(all_columns, res)]
     pattern = re.compile("^blank_header_\d+$")
@@ -327,7 +316,7 @@ def predict(**kwargs):
         for item in pred_list
         for header, pred in item.items()
     ]
-    # print(f"{prediction_list=}")
+    # print(f"{pred_labels=}")
     return [
         {
             "inputDataSource": f"{dataset_id}:0",
@@ -347,10 +336,11 @@ def predict(**kwargs):
 #         "Child 2 DOB",
 #         "Ch1.LastName",
 #         "ACC Effective Date",
-#         "Member_Information_Employee_Benefit_Class"
-#         # "Member_Information_Last_Name",
-#         # "blank_header_1",
-#         # "blank_header_20",
+#         "Member_Information_Employee_Benefit_Class",
+#         "Employee_Address_1",
+#         "Member_Information_Last_Name",
+#         "blank_header_1",
+#         "blank_header_20",
 #     ]
 #     # columns = []
 #     results = predict(
