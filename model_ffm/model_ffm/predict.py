e553aea726a1cd8651cdba7a98bb82f3aab90437
tariq-mohamed <Tariq.Mohamed@FINEOS.com>
2022-10-28 23:31:36 +0530

    Update predict.py

--- model_ffm/model_ffm/predict.py
+++ model_ffm/model_ffm/predict.py
@@ -2,10 +2,18 @@ def predict(**kwargs):
     import pandas as pd
     import numpy as np
     from inflection import humanize, underscore
-    import joblib
     import pkg_resources
     import re
     import onnxruntime
+
+    ARTIFACT_VERSION = "2.4.6"
+    MAX_SEQ_LENGTH = 16
+    # regex patterns for post prediction logic
+    PATTERN_DEP = re.compile("^dependent_relationship_dependent.+(\d)")
+    PATTERN_DEP1 = re.compile("^dependent(-\d)?")
+    PATTERN_CHILD = re.compile("^.+(\d)")
+    PATTERN_EMP_ADD = re.compile("^employee_address(_\d)?")
+
 
     dataset_id = kwargs.get("inputs").get("datasetId")
     if not kwargs.get("inputs").get("columns"):
@@ -17,32 +25,28 @@ def predict(**kwargs):
             }
         ]
 
-    def padarray(A):
-        t = 16 - len(A)
-        return np.pad(A, pad_width=(t, 0), mode="constant")
-
     def read_objs(name):
         res_loc = pkg_resources.resource_stream(
-            "model_ffm", "data/" + name + "2.4.6.pkl"
+            "model_ffm", f"data/{name}{ARTIFACT_VERSION}.pkl"
         )
         a = joblib.load(res_loc)
         return a
 
     def filter_bank(df):
-        _mask = (df["ent"].isin(["UNK"])) & ~df["head"].isin(
+        mask = (df["ent"].isin(["UNK"])) & ~df["head"].isin(
             ["GroupNumber", "AccountNumber"]
         )
-        _mask_1 = (df["ent"].isin(["Spouse", "Child", "Dependent"])) & (
+        mask_1 = (df["ent"].isin(["Spouse", "Child", "Dependent"])) & (
             df["prod"].isin(["VIS", "DEN"])
         )
-        _mask_9 = df["base"].isin(["Eligibility_DOH"])
-        _mask_8 = (df["base"].isin(["Employee_Address"])) & (df["head"] == "Zip")
-        _mask_10 = df["base"].isin(["Class/Set"])
-        _mask_13 = (df["base"].isin(["Employee_Address_3"])) & (
+        mask_9 = df["base"].isin(["Eligibility_DOH"])
+        mask_8 = (df["base"].isin(["Employee_Address"])) & (df["head"] == "Zip")
+        mask_10 = df["base"].isin(["Class/Set"])
+        mask_13 = (df["base"].isin(["Employee_Address_3"])) & (
             df["head"] == "AddressLine3"
         )
-        _mask_14 = df["head"] == "GF_BenefitAmount"
-        _mask_17 = (
+        mask_14 = df["head"] == "GF_BenefitAmount"
+        mask_17 = (
             df["base"].isin(
                 [
                     "Supplemental/Voluntary_Employee_Life_In_Force_Amount",
@@ -50,7 +54,7 @@ def predict(**kwargs):
                 ]
             )
         ) & (df["prod"] == "LIFESUP")
-        _mask_18 = (
+        mask_18 = (
             df["base"].isin(
                 [
                     "Supplemental/Voluntary_Employee_AD&D_Applied_For_Amount",
@@ -58,7 +62,7 @@ def predict(**kwargs):
                 ]
             )
         ) & (df["prod"] == "ADDSUP")
-        _mask_20 = (
+        mask_20 = (
             df["base"].isin(
                 [
                     "EMPLOYEE_Voluntary/Supplemental_Coverage_LIFE_Benefit_Amount",
@@ -66,15 +70,15 @@ def predict(**kwargs):
                 ]
             )
         ) & (df["prod"] == "LIFEVOL")
-        _mask_21 = (
+        mask_21 = (
             df["base"].isin(["Dependent_Relationship_Dependent_Relationship"])
         ) & (df["ent"] == "Dependent")
-        _mask_23 = (
+        mask_23 = (
             (df["head"].isin(["AccountNumber"]))
             & (df["ent"] == "Primary")
             & (df["prod"] == "UNK")
         )
-        _mask_24 = (
+        mask_24 = (
             (
                 df["base"].isin(
                     ["Basic_Life_In_Force_Amount", "Basic_Life_Applied_For_Amount"]
@@ -83,22 +87,22 @@ def predict(**kwargs):
             & (df["ent"] == "Primary")
             & (df["prod"] == "LTD")
         )
-        _mask_25 = (
+        mask_25 = (
             (df["base"].isin(["Supplemental/Voluntary_Employee_Life_Effective_Date"]))
             & (df["head"].isin(["EffectiveDate"]))
             & (df["prod"] == "LIFESUP")
         )
-        _mask_27 = (
+        mask_27 = (
             (~df["prod"].isin(["UNK", "primary"]))
             & (df["head"].isin(["BinaryResponse"]))
             & (df["ent"] == "Primary")
         )
-        _mask_28 = (
+        mask_28 = (
             (~df["prod"].isin(["UNK"]))
             & (df["head"].isin(["UNK"]))
             & (df["ent"] == "Primary")
         )
-        _mask_29 = (
+        mask_29 = (
             (df["prod"].isin(["LIFESUP"]))
             & (df["head"].isin(["Inforce Amount"]))
             & (df["ent"] == "Primary")
@@ -108,68 +112,63 @@ def predict(**kwargs):
                 )
             )
         )
-        _mask_31 = (
+        mask_31 = (
             (df["prod"].isin(["ADDSUP"]))
             & (df["head"].isin(["EffectiveDate"]))
             & (df["ent"] == "Primary")
             & (df["base"].isin(["Supplemental/Voluntary_Employee_AD&D_Effective_Date"]))
         )
-        _mask_32 = (
+        mask_32 = (
             (df["prod"].isin(["UNK"]))
             & (df["head"].isin(["Zip"]))
             & (df["ent"] == "Primary")
             & (df["base"].isin(["Employee_Address_3"]))
         )
-        _mask_34 = (
+        mask_34 = (
             (df["prod"].isin(["UNK"]))
             & (df["head"].isin(["UNK"]))
             & (df["ent"] == "Primary")
         )
-        _mask_f1 = (df["ent"].isin(["Child"])) & df["prod"].isin(["CHLIFE", "CHADD"])
+        mask_f1 = (df["ent"].isin(["Child"])) & df["prod"].isin(["CHLIFE", "CHADD"])
 
-        df.loc[:, "prod"] = df["prod"].where(~(_mask_1), other="UNK")
-        df.loc[:, "head"] = df["head"].where(~(_mask_8), other="AddressLine1")
-        df.loc[:, "head"] = df["head"].where(~(_mask_9), other="HireDate")
-        df.loc[:, "head"] = df["head"].where(~(_mask_10), other="BenefitClass")
-        df.loc[:, "head"] = df["head"].where(~(_mask_13), other="Zip")
-        df.loc[:, "head"] = df["head"].where(~(_mask_14), other="Grandfathered Amount")
-        df.loc[:, "prod"] = df["prod"].where(~(_mask_17), other="LIFEVOL")
-        df.loc[:, "prod"] = df["prod"].where(~(_mask_18), other="ADDVOL")
-        df.loc[:, "prod"] = df["prod"].where(~(_mask_20), other="LIFESUP")
-        df.loc[:, "ent"] = df["ent"].where(~(_mask_21), other="Spouse")
-        df.loc[:, "ent"] = df["ent"].where(~(_mask_23), other="UNK")
-        df.loc[:, "prod"] = df["prod"].where(~(_mask_24), other="LIFE")
-        df.loc[:, "prod"] = df["prod"].where(~(_mask_25), other="LIFEVOL")
-        df.loc[:, "head"] = df["head"].where(~(_mask_27), other="Applied For Amount")
-        df.loc[:, "ent"] = df["ent"].where(~(_mask_28), other="UNK")
-        df.loc[:, "prod"] = df["prod"].where(~(_mask_29), other="LIFEVOL")
-        df.loc[:, "prod"] = df["prod"].where(~(_mask_31), other="ADDVOL")
-        df.loc[:, "head"] = df["head"].where(~(_mask_32), other="State")
-        df.loc[:, "ent"] = df["ent"].where(~(_mask_34), other="UNK")
-        df.loc[:, "ent"] = df["ent"].where(~(_mask), other="Primary")
-        df.loc[:, "ent"] = df["ent"].where(~(_mask_f1), other="Primary")
+        df.loc[:, "prod"] = df["prod"].where(~(mask_1), other="UNK")
+        df.loc[:, "head"] = df["head"].where(~(mask_8), other="AddressLine1")
+        df.loc[:, "head"] = df["head"].where(~(mask_9), other="HireDate")
+        df.loc[:, "head"] = df["head"].where(~(mask_10), other="BenefitClass")
+        df.loc[:, "head"] = df["head"].where(~(mask_13), other="Zip")
+        df.loc[:, "head"] = df["head"].where(~(mask_14), other="Grandfathered Amount")
+        df.loc[:, "prod"] = df["prod"].where(~(mask_17), other="LIFEVOL")
+        df.loc[:, "prod"] = df["prod"].where(~(mask_18), other="ADDVOL")
+        df.loc[:, "prod"] = df["prod"].where(~(mask_20), other="LIFESUP")
+        df.loc[:, "ent"] = df["ent"].where(~(mask_21), other="Spouse")
+        df.loc[:, "ent"] = df["ent"].where(~(mask_23), other="UNK")
+        df.loc[:, "prod"] = df["prod"].where(~(mask_24), other="LIFE")
+        df.loc[:, "prod"] = df["prod"].where(~(mask_25), other="LIFEVOL")
+        df.loc[:, "head"] = df["head"].where(~(mask_27), other="Applied For Amount")
+        df.loc[:, "ent"] = df["ent"].where(~(mask_28), other="UNK")
+        df.loc[:, "prod"] = df["prod"].where(~(mask_29), other="LIFEVOL")
+        df.loc[:, "prod"] = df["prod"].where(~(mask_31), other="ADDVOL")
+        df.loc[:, "head"] = df["head"].where(~(mask_32), other="State")
+        df.loc[:, "ent"] = df["ent"].where(~(mask_34), other="UNK")
+        df.loc[:, "ent"] = df["ent"].where(~(mask), other="Primary")
+        df.loc[:, "ent"] = df["ent"].where(~(mask_f1), other="Primary")
 
         return df
 
     def regex_filter_bank(row):
-        pattern_dep = re.compile("^dependent_relationship_dependent.+(\d)")
-        pattern_dep1 = re.compile("^dependent(-\d)?")
-        pattern_child = re.compile("^.+(\d)")
-        pattern_emp_add = re.compile("^employee_address(_\d)?")
+        match_obj_emp_add = PATTERN_EMP_ADD.match(row["base"].lower())
 
-        match_obj_emp_add = pattern_emp_add.match(row["base"].lower())
-
-        if pattern_dep.match(row["base"].lower()):
+        if PATTERN_DEP.match(row["base"].lower()):
             if (
-                pattern_dep1.match(row["ent"].lower())
+                PATTERN_DEP1.match(row["ent"].lower())
                 and row["head"] == "Relationship"
                 and row["prod"] == "UNK"
             ):
-                row["ent"] = "Child-" + str(pattern_dep.match(row["base"].lower())[1])
+                row["ent"] = "Child-" + PATTERN_DEP.match(row["base"].lower())[1]
 
         if row["ent"] == "Child" and row["prod"] == "UNK":
-            if pattern_child.match(row["base"]):
-                row["ent"] = "Child-" + str(pattern_child.match(row["base"].lower())[1])
+            if match_obj := PATTERN_CHILD.match(row["base"].lower()):
+                row["ent"] = "Child-" + match_obj[1]
 
         if match_obj_emp_add:
             if match_obj_emp_add[1]:
@@ -188,18 +187,15 @@ def predict(**kwargs):
     head_dict = read_objs("head_dict")
     party_dict = read_objs("entity_dict")
 
-    max_seq_len = 16
-    pad = "pre"
-
     # load wordPiece tokenizer
     tokenizer = pkg_resources.resource_stream(
-        "model_ffm", "data/tokenzier_v2.4.6.joblib"
+        "model_ffm", f"data/tokenzier_v{ARTIFACT_VERSION}.joblib"
     )
     bert_wp_loaded = joblib.load(tokenizer)
 
     # load trained model
     fp = pkg_resources.resource_filename(
-        "model_ffm", "data/ffm_model_torch_v2.4.6.onnx"
+        "model_ffm", f"data/ffm_model_torch_v{ARTIFACT_VERSION}.onnx"
     )
     loaded_model = onnxruntime.InferenceSession(fp)
 
@@ -217,22 +213,14 @@ def predict(**kwargs):
         processed_cols.append(column)
         token_ids_test.append(bert_wp_loaded.encode(column).ids)
 
-    # test_tokens = pad_sequences(
-    #     token_ids_test,
-    #     padding=pad,
-    #     value=bert_wp_loaded.get_vocab()["[PAD]"],
-    #     maxlen=max_seq_len,
-    # )
     pred = []
 
-    for A in range(len(token_ids_test)):
-        t = max_seq_len - len(token_ids_test[A])
+    for token_id in token_ids_test:
+        t = MAX_SEQ_LENGTH - len(token_id)
         if t >= 0:
-            i = np.pad(token_ids_test[A], pad_width=(t, 0), mode="constant").astype(
-                np.int32
-            )
+            i = np.pad(token_id, pad_width=(t, 0), mode="constant").astype(np.int32)
         else:
-            i = np.array(token_ids_test[A][0:16]).astype(np.int32)
+            i = np.array(token_id[0:16]).astype(np.int32)
         i = i.reshape(1, -1)
         pred.append(
             loaded_model.run([label_name1, label_name2, label_name3], {input_name: i})
@@ -252,11 +240,11 @@ def predict(**kwargs):
     predictions_test.append(predictions_test_1)
     predictions_test.append(predictions_test_2)
 
-    r1_len = len(all_columns)
+    run_len = len(all_columns)
 
-    p1_test = np.argmax(np.array(predictions_test[0]).reshape(r1_len, -1), axis=1)
-    p2_test = np.argmax(np.array(predictions_test[1]).reshape(r1_len, -1), axis=1)
-    p3_test = np.argmax(np.array(predictions_test[2]).reshape(r1_len, -1), axis=1)
+    p1_test = np.argmax(np.array(predictions_test[0]).reshape(run_len, -1), axis=1)
+    p2_test = np.argmax(np.array(predictions_test[1]).reshape(run_len, -1), axis=1)
+    p3_test = np.argmax(np.array(predictions_test[2]).reshape(run_len, -1), axis=1)
 
     prod_label = np.array([prod_dict[x] for x in p1_test]).reshape((-1, 1))
     header_label = np.array([head_dict[x] for x in p2_test]).reshape((-1, 1))
@@ -269,15 +257,15 @@ def predict(**kwargs):
 
     prob_1 = softmax_layer.run(
         [label_name_x],
-        {input_name_x: np.array(predictions_test[0]).reshape(r1_len, -1)},
+        {input_name_x: np.array(predictions_test[0]).reshape(run_len, -1)},
     )[0]
     prob_2 = softmax_layer.run(
         [label_name_x],
-        {input_name_x: np.array(predictions_test[1]).reshape(r1_len, -1)},
+        {input_name_x: np.array(predictions_test[1]).reshape(run_len, -1)},
     )[0]
     prob_3 = softmax_layer.run(
         [label_name_x],
-        {input_name_x: np.array(predictions_test[2]).reshape(r1_len, -1)},
+        {input_name_x: np.array(predictions_test[2]).reshape(run_len, -1)},
     )[0]
 
     prod_confidence = np.array(
@@ -306,7 +294,6 @@ def predict(**kwargs):
         [(a, b) for a, b in zip(ll, cl)]
         for ll, cl in zip(new_labels, confidences)  # noqa
     ]
-
     pred_list = [{entity: prediction} for entity, prediction in zip(all_columns, res)]
     pattern = re.compile("^blank_header_\d+$")
     unknown_triplet = [("UNK", 1.0), ("UNK", 1.0), ("UNK", 1.0)]
@@ -343,7 +330,6 @@ def predict(**kwargs):
 #         "blank_header_1",
 #         "blank_header_20",
 #     ]
-#     # columns = []
 #     results = predict(
 #         model_name="model_ffm",
 #         artifacts=["data/bert_wp_tok_updated_v2.joblib"],
